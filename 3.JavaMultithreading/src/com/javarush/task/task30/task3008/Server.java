d6988105521098d7e823167ea1b548a699daf7b8
Raizer4 <nikitakraska1@gmail.com>
2024-06-19 09:09:58 +0300

    level-36-final

--- 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3008/Server.java
@@ -1,7 +1,38 @@
 package com.javarush.task.task30.task3008;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Map;
+
 public class Server {
-    public static void main(String[] args) {
-        
+
+    private static Map<String, Connection> connectionMap;
+
+    public static void main(String[] args) throws IOException {
+
+        int port = ConsoleHelper.readInt();
+        try (ServerSocket socket = new ServerSocket(port)) {
+            ConsoleHelper.writeMessage("Сервер запущен");
+
+            while (true) {
+                Socket accept = socket.accept();
+                Handler handler = new Handler(accept);
+                handler.start();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+}
+
+    private static class Handler extends Thread {
+        private Socket socket;
+        public Handler(Socket socket){
+            this.socket = socket;
+        }
+
     }
+
 }
