a13ade8a552976c13a9c0dc2045e6e5fc7b3a4da
Raizer4 <nikitakraska1@gmail.com>
2024-08-18 21:24:29 +0300

    level-48-final

--- 4.JavaCollections/src/com/javarush/task/task38/task3810/Changelog.java
+++ 4.JavaCollections/src/com/javarush/task/task38/task3810/Changelog.java
@@ -1,8 +1,6 @@
 package com.javarush.task.task38.task3810;
 
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
 
 public @interface Changelog {
-    //напиши свой код
+    Revision[] value() default {};
 }
